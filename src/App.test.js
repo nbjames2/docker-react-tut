057822cac5890b4200f5508e4eb3b34292508643
NICK JAMES <32378305+nbjames2@users.noreply.github.com>
2020-05-19 08:48:10 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/I was changed!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I was changed!/i);
   expect(linkElement).toBeInTheDocument();
 });
